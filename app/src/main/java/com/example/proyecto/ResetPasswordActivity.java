b11931f6e157edbbcf369ee60dcdb98a916d6b2f
paolapv123@gmail.com <paolapv123@gmail.com>
2022-07-18 21:56:45 -0600

    Reset password ahora tiene mensaje cuando se envia el correo

--- app/src/main/java/com/example/proyecto/ResetPasswordActivity.java
+++ app/src/main/java/com/example/proyecto/ResetPasswordActivity.java
@@ -5,14 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.proyecto.Helper.ErrorMessages;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class ResetPasswordActivity extends AppCompatActivity {
@@ -26,6 +29,9 @@ public class ResetPasswordActivity extends AppCompatActivity {
     //Progress para mostrar que se esta realizando el envio del correo
     private ProgressDialog mDialog;
 
+    //private ProgressDialog mDialog2;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +42,7 @@ public class ResetPasswordActivity extends AppCompatActivity {
 
         //Instanciar el ProgressDialog
         mDialog = new ProgressDialog(this);
+        //mDialog2 = new ProgressDialog(this);
 
         //Instanciar con el Id de cada uno
         mEditTextEmail = findViewById(R.id.editTextEmail);
@@ -75,7 +82,10 @@ public class ResetPasswordActivity extends AppCompatActivity {
             public void onComplete(@NonNull Task<Void> task) {
 
                 if(task.isSuccessful()){
-                    Toast.makeText(ResetPasswordActivity.this, "Se ha enviado un correo para restablecer tu contraseña", Toast.LENGTH_SHORT).show();
+                    Snackbar mySnackbarResetPassword = Snackbar.make(findViewById(R.id.myCoordinatorLayout), "Se ha enviado un correo para restablecer tu contraseña. Revisa tu bandeja de entrada o Spam.", Snackbar.LENGTH_SHORT);
+                    mySnackbarResetPassword.show();
+                    mEditTextEmail.setText("");
+                    ErrorMessages.sendToLogin(ResetPasswordActivity.this);
                 }
                 else{
                     Toast.makeText(ResetPasswordActivity.this, "Error al enviar correo para restablecer contraseña", Toast.LENGTH_SHORT).show();
@@ -85,11 +95,4 @@ public class ResetPasswordActivity extends AppCompatActivity {
             }
         });
     }
-
-
-
-
-
-
-
 }
